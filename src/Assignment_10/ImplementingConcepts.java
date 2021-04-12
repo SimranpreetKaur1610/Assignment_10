0c2f0ea03fb7e4d838350bfd5d83e49952e6574d
Simranpreetkaur <harsim610@gmail.com>
2021-04-12 01:51:15 -0400

    initial Commit

--- src/Assignment_10/ImplementingConcepts.java
+++ src/Assignment_10/ImplementingConcepts.java
@@ -1,8 +1,15 @@
 package Assignment_10;
 
+import java.util.Scanner;
+
 public class ImplementingConcepts {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner read = new Scanner(System.in);
+        System.out.print("Enter the monthly sales : ");
+        double sales = read.nextDouble();
+        System.out.print("Enter the amount of advanced pay : ");
+        double advance = read.nextDouble();
+
     }
 }
